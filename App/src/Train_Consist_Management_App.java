749a44c8a3cc677d7ec5058848ca6b489e1e7dd0
LakshanaAi <lakshanababu3012@gmail.com>
2026-04-24 09:32:19 +0530

    Juzer SM Added UC2

--- App/src/Train_Consist_Management_App.java
+++ App/src/Train_Consist_Management_App.java
@@ -26,5 +26,53 @@ public class Train_Consist_Management_App {
         List<String> trainConsist = new ArrayList<>();
         System.out.println("Initial Bogie Count: " + trainConsist.size());
         System.out.println("Train consist initialized succesfully");
+/**
+ * ================================================================
+ * MAIN CLASS - UseCase2TrainConsist=Mgmnt
+ * ================================================================
+ *
+ * Use case 2 : Add Passenger Bogies to Train
+ *
+ * Description:
+ * This class demonstrates how passenger bogies can be managed using ArrayList operations.
+ *
+ * At this stage, the application:
+ * -Adds new boggies to the train
+ * -Removes existing bogies
+ * -Checks for bogie Availability
+ * -Displays the final consist
+ *
+ * This maps CRUD operations using ArrayList.
+  */    System.out.println("UC2: Add Passenger Bogies to Train");
+        System.out.println();
+
+
+        trainConsist.add("Sleeper");
+        trainConsist.add("AC Chair");
+        trainConsist.add("First Class");
+
+
+        System.out.println("Passenger Bogies after addition:");
+        System.out.println(trainConsist);
+
+
+        trainConsist.remove("AC Chair");
+        System.out.println("\nAfter removing 'AC Chair':");
+        System.out.println(trainConsist);
+
+
+        if (trainConsist.contains("Sleeper")) {
+            System.out.println("\n'Sleeper' bogie exists in the train.");
+        } else {
+            System.out.println("\n'Sleeper' bogie does NOT exist.");
+        }
+
+        System.out.println("\nFinal Train Consist:");
+        System.out.println(trainConsist);
+
+
+        System.out.println("\nProgram executed successfully.");
     }
 }
+
+
